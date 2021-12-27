ffca710f31bcf79ff42624b9aa48755ec6a4c851
nmamatsashvili <nmamatsashvili@outlook.com>
2021-12-27 10:21:52 +0400

    Add project files.

---
+++ WebApp/Controllers/Weather.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    [ApiController, Route("api/[controller]")]
+    [Consumes("application/json")]
+    public class Weather : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetWeather(string city, int daysCount) {
+            if (string.IsNullOrEmpty(city) || string.IsNullOrWhiteSpace(city))
+                return BadRequest("city is not provided");
+            if (daysCount < 0 || daysCount > 16)
+                return BadRequest("incorrect days count requested");
+            WorkerApp.WeatherClass weather = new WorkerApp.WeatherClass();
+            bool isSuccessRequest;
+            var result = weather.getWeather(city, daysCount, out isSuccessRequest);
+            if (isSuccessRequest)
+                BadRequest("unexpected error occured.");
+            return Ok(result);
+        }
+    }
+}
---
+++ WorkerApp/WeatherClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+
+namespace WorkerApp
+{
+    public class WeatherClass
+    {
+        public object getWeather(string city, int daysCount, out bool success) {
+            try
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://api.openweathermap.org/data/2.5/forecast/daily?q={city}&cnt={daysCount}&units=metric&appid=886705b4c1182eb1c69f28eb8c520e20"),
+                };
+                using (var response = client.SendAsync(request).Result)
+                {
+                    response.EnsureSuccessStatusCode();
+                    success = true;
+                    return response.Content.ReadAsStringAsync().Result;
+                }
+            }
+            catch (Exception ex)
+            {
+                success = false;
+                return ex.Message;
+            }
+        }
+    }
+}
